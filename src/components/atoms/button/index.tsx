b78cb518bfc62c2785256465935f739a3c8a6565
kimseonggon <shuw75@gmail.com>
2025-04-09 23:49:43 +0900

    button 정리

---
+++ src/components/atoms/button/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useEffect } from 'react';
+import styles from './style.module.scss'
+import { makeStyles } from "@mui/material/styles"
+import Button from '@mui/material/Button'
+interface Props {
+  label: string
+  variant?: 'text' | 'outlined' | 'contained'
+  type?: string
+  disabled?: boolean
+  onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void
+
+}
+const ButtonComponent = (props: Props) => {
+  const { label, type, variant = 'contained', disabled = false, onClick } = props
+  const [disabledToUse, setDisabledToUse] = useState(disabled)
+  const lastReceivedDisabled = useRef(disabled)
+  const timeout = useRef(null)
+
+  useEffect(() => {
+    setDisabledToUse(disabled)
+    lastReceivedDisabled.current = disabled
+
+
+    return () => {
+      if (timeout.current) {
+        // 타이머 발동 전 컴포넌트가 사라진다면 타이머를 끄고 사라진다.
+        clearTimeout(timeout.current)
+      }
+    }
+  }, [disabled])
+
+  /**
+   * 연속 클릭 방지
+   */
+  const wrappedOnClick = (e?: React.MouseEvent<HTMLButtonElement>) => {
+    setDisabledToUse(true)
+    onClick(e)
+    setTimeout(() => {
+      setDisabledToUse(false)
+    }, 500)
+  }
+
+  const className = () => {
+    let name = ``
+    if (type) {
+      name = type.split(' ').map(t => {
+        console.log('t', t)
+
+        return styles[t]
+      }).join(' ')
+    }
+    return name
+  }
+
+  return (
+    <Button
+      className={className()}
+      variant={variant}
+      disabled={disabledToUse}
+      onClick={wrappedOnClick}
+    >{label}</Button>
+  );
+};
+
+export default ButtonComponent;
--- src/components/pages/ui/index.tsx
+++ src/components/pages/ui/index.tsx
@@ -1,10 +1,161 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './style.module.scss'
+import ButtonComponent from '../../atoms/button'
+import { buttonType } from '../../../enum';
 
 const UIConponent = () => {
+  const [value, setValue] = useState('')
+  const textarea = useRef<HTMLTextAreaElement>(null)
+
   return (
     <div className={styles.ui}>
-      ui
+      <div className={styles.row}>
+        <ButtonComponent
+          label='XLARGE_LOW'
+          type={buttonType.XLARGE_LOW}
+          onClick={() => {
+            setValue(`<ButtonComponent label='XLARGE_LOW' type={buttonType.XLARGE_LOW} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='XLARGE_MIDDLE'
+          type={buttonType.XLARGE_MIDDLE}
+          onClick={() => {
+            setValue(`<ButtonComponent label='XLARGE_MIDDLE' type={buttonType.XLARGE_MIDDLE} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='XLARGE_HIGH'
+          type={buttonType.XLARGE_HIGH}
+          onClick={() => {
+            setValue(`<ButtonComponent label='XLARGE_HIGH' type={buttonType.XLARGE_HIGH} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='XLARGE_MIDDLE_ERROR'
+          type={buttonType.XLARGE_MIDDLE_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='XLARGE_MIDDLE_ERROR' type={buttonType.XLARGE_MIDDLE_ERROR} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='XLARGE_HIGH_ERROR'
+          type={buttonType.XLARGE_HIGH_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='XLARGE_HIGH_ERROR' type={buttonType.XLARGE_HIGH_ERROR} onClick={() => {}} />`)
+          }} />
+      </div>
+      <div className={styles.row}>
+        <ButtonComponent
+          label='LARGE_LOW'
+          type={buttonType.LARGE_LOW}
+          onClick={() => {
+            setValue(`<ButtonComponent label='LARGE_LOW' type={buttonType.LARGE_LOW} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='LARGE_MIDDLE'
+          type={buttonType.LARGE_MIDDLE}
+          onClick={() => {
+            setValue(`<ButtonComponent label='LARGE_MIDDLE' type={buttonType.LARGE_MIDDLE} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='LARGE_HIGH'
+          type={buttonType.LARGE_HIGH}
+          onClick={() => {
+            setValue(`<ButtonComponent label='LARGE_HIGH' type={buttonType.LARGE_HIGH} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='LARGE_MIDDLE_ERROR'
+          type={buttonType.LARGE_MIDDLE_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='LARGE_MIDDLE_ERROR' type={buttonType.LARGE_MIDDLE_ERROR} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='LARGE_HIGH_ERROR'
+          type={buttonType.LARGE_HIGH_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='LARGE_HIGH_ERROR' type={buttonType.LARGE_HIGH_ERROR} onClick={() => {}} />`)
+          }} />
+      </div>
+
+      <div className={styles.row}>
+        <ButtonComponent
+          label='MEDIUM_LOW'
+          type={buttonType.MEDIUM_LOW}
+          onClick={() => {
+            setValue(`<ButtonComponent label='MEDIUM_LOW' type={buttonType.MEDIUM_LOW} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='MEDIUM_MIDDLE'
+          type={buttonType.MEDIUM_MIDDLE}
+          onClick={() => {
+            setValue(`<ButtonComponent label='MEDIUM_MIDDLE' type={buttonType.MEDIUM_MIDDLE} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='MEDIUM_HIGH'
+          type={buttonType.MEDIUM_HIGH}
+          onClick={() => {
+            setValue(`<ButtonComponent label='MEDIUM_HIGH' type={buttonType.MEDIUM_HIGH} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='MEDIUM_MIDDLE_ERROR'
+          type={buttonType.MEDIUM_MIDDLE_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='MEDIUM_MIDDLE_ERROR' type={buttonType.MEDIUM_MIDDLE_ERROR} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='MEDIUM_HIGH_ERROR'
+          type={buttonType.MEDIUM_HIGH_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='MEDIUM_HIGH_ERROR' type={buttonType.MEDIUM_HIGH_ERROR} onClick={() => {}} />`)
+          }} />
+      </div>
+
+      <div className={styles.row}>
+        <ButtonComponent
+          label='SMALL_LOW'
+          type={buttonType.SMALL_LOW}
+          onClick={() => {
+            setValue(`<ButtonComponent label='SMALL_LOW' type={buttonType.SMALL_LOW} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='SMALL_MIDDLE'
+          type={buttonType.SMALL_MIDDLE}
+          onClick={() => {
+            setValue(`<ButtonComponent label='SMALL_MIDDLE' type={buttonType.SMALL_MIDDLE} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='SMALL_HIGH'
+          type={buttonType.SMALL_HIGH}
+          onClick={() => {
+            setValue(`<ButtonComponent label='SMALL_HIGH' type={buttonType.SMALL_HIGH} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='SMALL_MIDDLE_ERROR'
+          type={buttonType.SMALL_MIDDLE_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='SMALL_MIDDLE_ERROR' type={buttonType.SMALL_MIDDLE_ERROR} onClick={() => {}} />`)
+          }} />
+        <ButtonComponent
+          label='SMALL_HIGH_ERROR'
+          type={buttonType.SMALL_HIGH_ERROR}
+          onClick={() => {
+            setValue(`<ButtonComponent label='SMALL_HIGH_ERROR' type={buttonType.SMALL_HIGH_ERROR} onClick={() => {}} />`)
+          }} />
+      </div>
+
+      <div>
+        <h4>소스보기</h4>
+        <textarea style={{ height: '50px' }} value={value} ref={textarea} />
+
+        <ButtonComponent
+          label='copy'
+          type={buttonType.SMALL_LOW}
+          onClick={() => {
+            if (textarea.current) {
+              textarea.current.select()
+              document.execCommand('copy')
+
+            }
+          }} />
+
+      </div>
     </div>
   );
 };
